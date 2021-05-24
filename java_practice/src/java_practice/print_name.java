99caba442cb8902bbe03b00374a051237f7a8fca
nidhibhrdwj <nidhibhrdwj@gmail.com>
2021-05-24 17:05:35 -0400

    added files

---
+++ java_practice/src/java_practice/print_name.java
@@ -0,0 +1,22 @@
+package java_practice;
+
+public class print_name {
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=19;
+		int x[]= new int[5];
+		String name[]= new String[3];
+		x[1]=2;
+		x[3]=5;
+		name[1]="nidhi";
+		name[2]="Bhardwaj";
+		System.out.println("my name is nidhi");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(x[3]);
+		System.out.println("my name is ");
+	}
+	
+
+}
